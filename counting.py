79a295341a52fb9f7aeedea0518be76fb214c736
Mahmoud Hossam <mahmoudhossam@users.noreply.github.com>
2011-05-25 18:29:45 +0200

    Initial Commit

---
+++ counting.py
@@ -0,0 +1,7 @@
+def start_count(end, begin = 1):
+	for i in range(begin, end+1):
+		print i
+		
+a = int(raw_input('Enter a number: '))
+
+start_count(a)
---
+++ test.py
@@ -0,0 +1,11 @@
+print("hello there!")
+while True :
+	print("\n\nEnter a number: ")
+	x = input()
+	print("Enter another number: ")
+	y = input()
+	if x > y:
+		print("good boy")
+		break
+	else :
+		print("get the hell outta here!")
