c098d8f4752782ccb69ea914bf66e5eadbb1e1c2
Mahmoud Hossam <mahmoudhossam@users.noreply.github.com>
2011-06-05 12:16:17 +0200

    modified sqrt.py, it now calculates a rough estimate of the square root of a given number.

--- sqrt.py
+++ sqrt.py
@@ -1,2 +1,15 @@
-def square(x):
-	return x**2
+def getsqrt(x):
+	l = 0
+	while square(l) < x:
+		l = 1 + l
+	return l
+
+def square(n):
+	return pow(n, 2)
+
+def	main():
+	f = int(raw_input('Enter a number: '))
+	print 'The square root of this number is', getsqrt(f)
+
+if __name__ == "__main__":
+	main()
